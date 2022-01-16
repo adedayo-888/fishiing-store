4f60ec3a052c902764d1963b06a4e3d7a0b00cf3
adedayo888 <dayoadeniji69@gmail.com>
2022-01-16 10:47:55 +0000

    refactored code

---
+++ build/precache-manifest.ab7de871cf85f2dcff2e8d1b3a655ab0.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5547e3bfb0767251ef9a08dcf1065c93",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e7a084d8dd8c0d1f748",
+    "url": "/static/css/2.437d98bc.chunk.css"
+  },
+  {
+    "revision": "e18176706427da45f48f",
+    "url": "/static/css/main.09f5e924.chunk.css"
+  },
+  {
+    "revision": "4e7a084d8dd8c0d1f748",
+    "url": "/static/js/2.53c84123.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.53c84123.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e18176706427da45f48f",
+    "url": "/static/js/main.a25a845b.chunk.js"
+  },
+  {
+    "revision": "ddf2906485b3597b7c32",
+    "url": "/static/js/runtime-main.1d05d419.js"
+  },
+  {
+    "revision": "7aa249ff1a7d6d967d733a1a4c0482e0",
+    "url": "/static/media/Zig-Zag-1.7aa249ff.svg"
+  },
+  {
+    "revision": "cc9f20596053a120cfee28dc3fba15a5",
+    "url": "/static/media/Zig-Zag.cc9f2059.svg"
+  },
+  {
+    "revision": "f2a929316746086db651312bcb1fd959",
+    "url": "/static/media/aboutUs.f2a92931.jpg"
+  },
+  {
+    "revision": "78097e65142311c64f1052e553fc29ef",
+    "url": "/static/media/contactUs.78097e65.jpg"
+  },
+  {
+    "revision": "3cccda2c071440af6cbcf4cc568085f4",
+    "url": "/static/media/myDefault.3cccda2c.jpg"
+  },
+  {
+    "revision": "9675841294261b773267d557f531a8ff",
+    "url": "/static/media/singleProductBcg.96758412.jpeg"
+  },
+  {
+    "revision": "46143c2f25bc4667fac18f5924288217",
+    "url": "/static/media/storeBcg.46143c2f.jpeg"
+  }
+]);
